5683f2774e146dac291e61ca01f293fc2397fbd4
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-11-10 16:00:44 +0430

    Add app/javascript/redux/ folder

---
+++ app/javascript/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import greetingReducer from "./features/greetingsSlice";
+const store = configureStore({
+    reducer : {
+        greeting: greetingReducer,
+    },
+});
+export default store;
